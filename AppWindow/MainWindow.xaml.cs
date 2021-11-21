02b68ef32e02b25dd6c5660fef1683d87a527fcc
roman <roman.izibaef2017@yandex.ru>
2021-11-21 21:43:53 +0500

    <v>

--- AppWindow/MainWindow.xaml.cs
+++ AppWindow/MainWindow.xaml.cs
@@ -1,28 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ConsoleApp1;
+using ConsoleApp1.Enums;
+using ConsoleApp1.Models;
+using ConsoleApp1.Services;
 
 namespace AppWindow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<ProductList> Products { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Products = new ObservableCollection<ProductList>
+            {
+                new ProductList{Id = 1, ImagePath = "/Images/product.jpg", Name = "Хлеб", Price= 34, Count = 0},
+                new ProductList{Id = 2, ImagePath = "/Images/product.jpg", Name = "Батон", Price= 29, Count = 0},
+                new ProductList{Id = 3, ImagePath = "/Images/product.jpg", Name = "Мука", Price= 56, Count = 0},
+                new ProductList{Id = 4, ImagePath = "/Images/product.jpg", Name = "Масло", Price= 121, Count = 0}
+            };
+            listProduct.ItemsSource = Products;
+        }
+        public class ProductList
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string ImagePath { get; set; }
+            public decimal Price { get; set; }
+            public int Count { get; set; }
+
+        }
+        private void plus(object sender, RoutedEventArgs e)
+        {
+            ProductList product = (ProductList)listProduct.ItemsSource;
+            product.Count = Convert.ToInt32(CountProduct.Text);
+            //a++;
+            //CountProduct.Text = a.ToString();
+            MessageBox.Show(CountProduct.Text);
+        }
+
+        private void minus(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(CountProduct.Text);
+            CountProduct.Text = Convert.ToString(a--);
+            MessageBox.Show(CountProduct.Text);
         }
 
         private void Order(object sender, RoutedEventArgs e)
@@ -37,46 +60,6 @@ namespace AppWindow
             Orders orders = new Orders();
             orders.Show();
             this.Close();
-        }
-
-        private void plus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void minus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void plus_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click12(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- ConsoleApp1/Models/ProductOrderModel.cs
+++ ConsoleApp1/Models/ProductOrderModel.cs
@@ -3,6 +3,7 @@ namespace ConsoleApp1.Models
 {
     public class ProductOrderModel
     {
+        public int Id { get; set; }
         public ProductModel productModels;
         public OrderModel OrderModel { get; set; }
         public ProductModel[] ProductModels { get; set; }
